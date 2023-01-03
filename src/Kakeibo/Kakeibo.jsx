34a941914d0e2ed2d264772712475a512a0c1370
ma-sa321 <attmptmine@gmail.com>
2023-01-03 20:52:46 +0900

    add: 入力項目にチェックをつける、localStorageを更新する

--- src/Kakeibo/Kakeibo.jsx
+++ src/Kakeibo/Kakeibo.jsx
@@ -1,22 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState, createContext } from "react";
 import { useForm } from "react-hook-form";
 import { EXPENSES } from "../const";
+import KakeiboItems from "../KakeiboItems/KakeiboItems";
 
+export const DataContext = createContext();
+
+/**
+ * 家計簿画面の全体
+ */
 const Kakeibo = () => {
   const [data, setData] = useState([]);
   const { register, handleSubmit, reset } = useForm();
 
-  const onSubmit = (value) => {
-    if (!value.title) return;
-    setData([
-      ...data,
-      {
-        key: getKey(),
-        title: value.title,
-        money: value.money,
-        expenses: value.expenses,
-      },
-    ]);
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem("kakeibo")));
+  }, []);
+
+  /** 登録 */
+  const onSubmit = (item) => {
+    if (!item.title | !item.money) return;
+
+    // localStorageがnullの場合、...dataがnullを許容出来ないので無理やりやっている
+    // TODO: リファクタリング
+    if (data) {
+      var inputData = [
+        ...data,
+        {
+          key: getKey(),
+          title: item.title,
+          money: item.money,
+          expenses: item.expenses,
+          isDelete: false,
+        },
+      ];
+    } else {
+      var inputData = [
+        {
+          key: getKey(),
+          title: item.title,
+          money: item.money,
+          expenses: item.expenses,
+          isDelete: false,
+        },
+      ];
+    }
+
+    setData(inputData);
+    var setJson = JSON.stringify(inputData);
+    localStorage.setItem("kakeibo", setJson);
     reset();
   };
 
@@ -24,31 +55,25 @@ const Kakeibo = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          タイトル
-          <input type="text" {...register("title")} />
-        </div>
-        <div>
-          金額
-          <input type="text" {...register("money")} />
-        </div>
-        <select {...register("expenses")}>
-          {EXPENSES.map((expense) => {
-            return <option value={expense.value}>{expense.label}</option>;
-          })}
-        </select>
-        <button type="submit">登録</button>
-      </form>
-      {data.map((value) => {
-        return (
-          <>
-            <div key={value.key}>
-              {value.title} / ¥{value.money}
-            </div>
-          </>
-        );
-      })}
+      <DataContext.Provider value={{ data, setData }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            タイトル
+            <input type="text" {...register("title")} />
+          </div>
+          <div>
+            金額
+            <input type="text" {...register("money")} />
+          </div>
+          <select {...register("expenses")}>
+            {EXPENSES.map((expense) => {
+              return <option value={expense.value}>{expense.label}</option>;
+            })}
+          </select>
+          <button type="submit">登録</button>
+        </form>
+        <KakeiboItems />
+      </DataContext.Provider>
     </>
   );
 };
---
+++ src/KakeiboItems/KakeiboItems.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import { DataContext } from "../Kakeibo/Kakeibo";
+
+/**
+ * 家計簿の表示部分
+ */
+const KakeiboItems = () => {
+  const { data, setData } = useContext(DataContext);
+  const deleteCheck = (checkedItem) => {
+    const deleteItem = data.map((item) => {
+      if (item.key === checkedItem.key) {
+        item.isDelete = !item.isDelete;
+      }
+      return item;
+    });
+    setData(deleteItem);
+    var setJson = JSON.stringify(data);
+    localStorage.setItem("kakeibo", setJson);
+  };
+
+  return data.map((item) => {
+    return <KakeiboItem key={item.key} item={item} onChange={deleteCheck} />;
+  });
+};
+
+const KakeiboItem = ({ item, onChange }) => {
+  const handleChange = () => {
+    onChange(item);
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="checkbox"
+          checked={item.isDelete}
+          onChange={handleChange}
+        />
+        <span>{item.title}</span>
+      </div>
+    </>
+  );
+};
+export default KakeiboItems;
